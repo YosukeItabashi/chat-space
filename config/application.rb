48257afcd8e27fab21e79cb85eb91272960d5d56
YosukeItabashi <yosuke.itabashi@itahashiyousukenoMacBook.local>
2017-04-29 16:24:40 +0900

    rails g controllerで無駄なhelperやassetsを生成しない

--- config/application.rb
+++ config/application.rb
@@ -13,3 +13,14 @@ module ChatSpace
     # -- all .rb files in that directory are automatically loaded.
   end
 end
+
+module YourApp
+  class Application < Rails::Application
+    #...
+    config.generators do |g|
+      g.assets     false
+      g.helper     false
+    end
+    #...
+  end
+end
