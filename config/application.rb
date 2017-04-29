8245d74e067bf94f6357b3d773c0a57d8d6e322b
YosukeItabashi <yosuke.itabashi@itahashiyousukenoMacBook.local>
2017-04-29 19:36:04 +0900

    rails g controllerで無駄なhelperやassetsを生成しない

--- config/application.rb
+++ config/application.rb
@@ -8,19 +8,9 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-  end
-end
-
-module YourApp
-  class Application < Rails::Application
-    #...
     config.generators do |g|
       g.assets     false
       g.helper     false
     end
-    #...
   end
 end
