22c59fc5049df81c3fc74009d62d34cfe9161ba2
YosukeItabashi <yosuke.itabashi@itahashiyousukenoMacBook.local>
2017-05-04 16:40:53 +0900

    Nameカラムの追加とインデックス貼り

--- db/migrate/20170504065514_devise_create_users.rb
+++ db/migrate/20170504065514_devise_create_users.rb
@@ -1,7 +1,9 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
+    add_index :users,  [:name, :email]
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name, null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
