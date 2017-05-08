258be17af887d270235c00ab630e0e780d350c7b
YosukeItabashi <yosuke.itabashi@itahashiyousukenoMacBook.local>
2017-05-08 12:43:40 +0900

    メンバーの追加機能の実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,30 @@
 class GroupsController < ApplicationController
 
+    def index
+        redirect_to :root
+    end
+
     def edit
     end
 
     def new
+      @group = Group.new
+    end
+
+    def create
+      @group = Group.new(group_params)
+      if @group.save
+        redirect_to :root
+      else
+        render 'new'
+      end
+    end
+
+    private
+
+    def group_params
+      params.require(:group).permit(:name)
+      # group_usersのDBに保存されないとだめじゃない？と思ってる
     end
 
 end
