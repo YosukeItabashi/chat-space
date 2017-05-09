f7035f3b464dcbb99dfe98a6582f2a5d99b393fd
YosukeItabashi <yosuke.itabashi@itahashiyousukenoMacBook.local>
2017-05-09 20:58:57 +0900

    messagesのindexアクションを定義

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,10 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user!
 
   def index
+    @groups = current_user.groups.limit(5)
+    @group = Group.find(params[:group_id])
+    @users = @group.users
   end
 
 end
