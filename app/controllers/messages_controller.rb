3df0eedbb8a811c77d45d935982f16bd11469657
YosukeItabashi <yosuke.itabashi@itahashiyousukenoMacBook.local>
2017-05-10 12:06:23 +0900

    最新5件を表示するように変更

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,7 @@
 class MessagesController < ApplicationController
 
   def index
-    @groups = current_user.groups.limit(5)
+    @groups = current_user.groups.order(id: :DESC).limit(5)
     @group = Group.find(params[:group_id])
     @users = @group.users
   end
