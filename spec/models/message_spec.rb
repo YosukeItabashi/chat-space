a4db3eee8a193641ef08040ef300fe5d1ba1c75a
YosukeItabashi <yosuke.itabashi@itahashiyousukenoMacBook.local>
2017-05-13 19:47:15 +0900

    Letを用いた記述の修正

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,16 +2,15 @@ require 'rails_helper'
 
 describe Message do
   describe '#create' do
-  let(:message) { FactoryGirl.build(:message) }
+    let(:message) { FactoryGirl.build(:message) }
 
     it "バリデーションに引っかかり、保存できない場合のテスト" do
-      message = build(:message, body: "")
+      message[:body] = ""
       message.valid?
       expect(message.errors[:body]).to include("を入力してください")
     end
 
     it "バリデーションに引っかからずに保存できる場合のテスト" do
-      message = build(:message, body: "これはテストです。")
       expect(message).to be_valid
     end
 
