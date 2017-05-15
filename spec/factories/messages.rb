0486f7502943601d3325001a69b7945d6e79a660
YosukeItabashi <yosuke.itabashi@itahashiyousukenoMacBook.local>
2017-05-15 17:33:42 +0900

    ファクトリーガールで値をセット

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,10 +1,7 @@
 FactoryGirl.define do
 
   factory :message do
-    body        Faker::Lorem.sentence
-    image       Faker::Internet.domain_name
-    group_id    Faker::Number.number(2)
-    user_id     Faker::Number.number(2)
-    created_at { Faker::Time.between(2.days.ago, Time.now, :all) }
+    body         { Faker::Name.name }
   end
+
 end
