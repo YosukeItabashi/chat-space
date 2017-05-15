62ac36f635c6097f455c6d520a2457b779226f4c
YosukeItabashi <yosuke.itabashi@itahashiyousukenoMacBook.local>
2017-05-15 18:09:26 +0900

    複数回使っている処理の定数化

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -6,47 +6,66 @@ describe MessagesController do
   let(:messages){create_list(:message, 3, user: user, group: group)}
 
 # indexアクションの検証
+# ログイン時
     describe 'GET #index' do
+      let(:params) { {group_id: group.id } }
 
-      it "＜ログイン中＞indexビューが表示されるか" do
+      before do
         login_user user
-        get :index, group_id: group
+      end
+
+      it "indexビューが表示されるか" do
+        get :index, params
         expect(response).to render_template :index
       end
 
-      it "＜非ログイン中＞new_user_session_pathへリダイレクトするか" do
-        get :index, group_id: group
-        expect(response).to redirect_to(new_user_session_path)
-      end
-
       it "@messagesの中身は期待した通りのものが取得できているか" do
-        login_user user
-        get :index, group_id: group
+        get :index, params
         expect(assigns(:messages)).to match(messages)
       end
+    end
 
+# 非ログイン時
+    describe 'GET #index' do
+      let(:params) { {group_id: group.id } }
+
+        it "new_user_session_pathへリダイレクトするか" do
+          get :index, params
+          expect(response).to redirect_to(new_user_session_path)
+        end
     end
 
 # createアクションの検証
+# ログイン時
     describe 'POST #create' do
+      let(:params) { {group_id: group.id, message: { body: "LGTM欲しさある" }} }
 
-      it "＜ログイン中＞group_messages_pathへ遷移するか" do
+      before do
         login_user user
-        post :create, group_id: group, message: {body: "テスト嫌い" }
-        expect(response).to redirect_to(group_messages_path)
       end
 
-      it "＜非ログイン中＞group_messages_pathへ遷移するか" do
-        post :create, group_id: group, message: {body: "テスト嫌い" }
-        expect(response).to redirect_to(new_user_session_path)
+      it "group_messages_pathへ遷移するか" do
+        login_user user
+        post :create, params
+        expect(response).to redirect_to(group_messages_path)
       end
 
       it "Messageの保存がされているか" do
         login_user user
         expect {
-          post :create, group_id: group, message: {body: "テスト嫌い" }
+          post :create, params
         }.to change(Message, :count).by(1)
       end
-
     end
+
+# 非ログイン時
+    describe 'POST #create' do
+      let(:params) { {group_id: group.id, message: { body: "LGTM欲しさある" }} }
+
+      it "＜非ログイン中＞group_messages_pathへ遷移するか" do
+        post :create, params
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
 end
