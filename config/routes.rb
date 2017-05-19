26a14c87fac95c915725fed3b6a745a1420e249d
YosukeItabashi <yosuke.itabashi@itahashiyousukenoMacBook.local>
2017-05-19 13:50:04 +0900

    resources :usersの中に入れてsearchのルーティングを定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   root 'groups#index'
   devise_for :users
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update] do
+    collection do
+      get 'search'
+    end
+  end
   resources :groups, except: [:destroy, :show] do
     resources :messages, only: [:index, :create]
   end
-  get 'users/search' => 'users#search'
+
 end
