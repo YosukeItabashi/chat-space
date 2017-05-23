20afdcd9322443f07ad710e1da371350a5634370
YosukeItabashi <yosuke.itabashi@itahashiyousukenoMacBook.local>
2017-05-23 11:49:59 +0900

    Storage :fileからfogへ

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,6 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
+  # S3 strage
   storage :fog
 
   # Override the directory where uploaded files will be stored.
